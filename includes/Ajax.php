1f0b6d094dfa7662aa7e4717c6aed4599b90d73c
Sadekur Rahman <37816541+sadekur@users.noreply.github.com>
2024-05-11 12:13:40 +0600

    Update Ajax.php

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -10,7 +10,7 @@ class Ajax {
 	/**
 	 * Class constructor
 	 */
-	function __construct() {
+	public function __construct() {
 		add_action( 'wp_ajax_priv_thrail_form', [ $this, 'submit_enquiry'] );
 		add_action( 'wp_ajax_nopriv_thrail_form', [ $this, 'submit_enquiry'] );
 	}
@@ -20,15 +20,18 @@ class Ajax {
 	 *
 	 * @return void
 	 */
-	public function submit_enquiry() {
-
-		$response = [
-			 'status'   => 0,
-			 'message'  =>__( 'Unauthorized!', 'codesigner' )
-		];
-
-		if( !wp_verify_nonce( $_POST['nonce'], 'thrail-crm') ) {
-			wp_send_json( $response );
+	public function handle_thrail_form() {
+	   	$response['status'] 	= 0;
+		$response['message'] 	= __( 'Something is wrong!', 'thrail-crm' );
+		
+		if( !wp_verify_nonce( $_POST['nonce'], 'thrail-crm' ) ) {
+			$response['message'] = __( 'Unauthorized!', 'thrail-crm' );
+		    wp_send_json( $response );
 		}
+
+	    $name = isset( $_POST['name'] ) ? sanitize_text_field( $_POST['name'] ) : '';
+		$email = isset( $_POST['email'] ) ? sanitize_text_field( $_POST['email'] ) : '';
+
+	    wp_send_json_success(['message' => 'Data processed successfully']);
 	}
 }
