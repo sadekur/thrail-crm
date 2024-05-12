dc50f48c4066bf2891c8e9428d10141c0e51a4cb
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-05-12 23:09:39 +0600

    updating with table

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -11,15 +11,15 @@ class Ajax {
 	}
 
 	public function handle_form_submission() {
-		// check_ajax_referer('thrail-admin-nonce', 'nonce');
+		check_ajax_referer('thrail-admin-nonce', 'nonce');
 
 		$response = [
 			 'status'	=> 0,
-			 'message'	=>__( 'Unauthorized!', 'codesigner' )
+			 'message'	=>__( 'Unauthorized!', 'thrail-crm' )
 		];
 
 		if( !wp_verify_nonce( $_POST['nonce'], 'thrail-admin-nonce' ) ) {
-		    wp_send_json( $response );
+			wp_send_json( $response );
 		}
 
 		if ( isset( $_POST['name'] ) && isset( $_POST['email'] ) ) {
@@ -27,17 +27,16 @@ class Ajax {
 			$email = sanitize_email( $_POST['email'] );
 
 			global $wpdb;
-			update_option('sdadd', $wpdb);
 			$table_name = $wpdb->prefix . 'thrail_crm_leads';
 
 			$email_exists = $wpdb->get_var($wpdb->prepare(
-                "SELECT COUNT(*) FROM $table_name WHERE email = %s",
-                $email
-            ));
-            if ($email_exists) {
-                wp_send_json_error(['message' => 'This email is already registered.']);
-                return;
-           	}
+				"SELECT COUNT(*) FROM $table_name WHERE email = %s",
+				$email
+			));
+			if ( $email_exists ) {
+				wp_send_json_error(['message' => 'This email is already registered.']);
+				return;
+			}
 
 			$wpdb->insert(
 				$table_name,
