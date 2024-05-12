63ae250e16ac11dc86a3c4fcebe865d76191e2c4
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-05-12 20:43:44 +0600

    update

--- includes/functions.php
+++ includes/functions.php
@@ -1,27 +1,34 @@
 <?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+	exit;
+}
 
 /**
- * Insert a new address
- *
- * @param  array  $args
- *
- * @return int|WP_Error
+ * Function to set up the database table for Thrail CRM
  */
+if( ! function_exists( 'thrail_crm_activate' ) ) :
+	function thrail_crm_activate() {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'thrail_crm_leads';
+		$charset_collate = $wpdb->get_charset_collate();
 
-register_activation_hook(__FILE__, 'thrail_crm_activate');
-function thrail_crm_activate() {
-    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			name tinytext NOT NULL,
+			email text NOT NULL,
+			time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			PRIMARY KEY  (id)
+		) $charset_collate;";
+
+		dbDelta($sql);
+	}
+endif;
+function thrail_crm_deactivate() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'thrail_crm_leads';
-    $charset_collate = $wpdb->get_charset_collate();
 
-    $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        name tinytext NOT NULL,
-        email text NOT NULL,
-        time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    dbDelta($sql);
+    $sql = "DROP TABLE IF EXISTS $table_name;";
+    $wpdb->query($sql);
 }
--- thrail-crm.php
+++ thrail-crm.php
@@ -20,6 +20,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
+register_activation_hook(__FILE__, 'thrail_crm_activate');
+register_deactivation_hook(__FILE__, 'thrail_crm_activate');
 
 /**
  * The main plugin class
@@ -93,22 +95,3 @@ function thrail_crm() {
 }
 
 thrail_crm();
-
-register_activation_hook(__FILE__, 'thrail_crm_activate');
-
-function thrail_crm_activate() {
-	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'thrail_crm_leads';
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		id mediumint(9) NOT NULL AUTO_INCREMENT,
-		name tinytext NOT NULL,
-		email text NOT NULL,
-		time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-		PRIMARY KEY  (id)
-	) $charset_collate;";
-
-	dbDelta($sql);
-}
