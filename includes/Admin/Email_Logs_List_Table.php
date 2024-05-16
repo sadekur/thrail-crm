33504c0fa3840f2b5c78c15f0b762aa170235316
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-05-17 02:09:24 +0600

    email log update with database

--- includes/Admin/Email_Logs_List_Table.php
+++ includes/Admin/Email_Logs_List_Table.php
@@ -2,8 +2,8 @@
 
 namespace Thrail\Crm\Admin;
 
-if (!class_exists('WP_List_Table')) {
-	require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+if (!class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
 class Email_Logs_List_Table extends \WP_List_Table {
@@ -18,9 +18,9 @@ class Email_Logs_List_Table extends \WP_List_Table {
 	public function get_columns() {
 		return [
 			'cb'        => '<input type="checkbox" />',
-			'name'      => __('Name', 'thrail-crm'),
-			'email'     => __('Email', 'thrail-crm'),
-			'time'      => __('Time', 'thrail-crm'),
+			'name'      => __( 'Name', 'thrail-crm' ),
+			'email'     => __( 'Email', 'thrail-crm' ),
+			'time'      => __( 'Time', 'thrail-crm' ),
 		];
 	}
 
@@ -28,35 +28,35 @@ class Email_Logs_List_Table extends \WP_List_Table {
 		$columns = $this->get_columns();
 		$hidden = [];
 		$sortable = [];
-		$this->_column_headers = [$columns, $hidden, $sortable];
+		$this->_column_headers = [ $columns, $hidden, $sortable ];
 
 		global $wpdb;
 		$table_name = $wpdb->prefix . 'thrail_crm_leads';
-		$perPage = 10;
-		$currentPage = $this->get_pagenum();
+		$per_page = 10;
+		$current_page = $this->get_pagenum();
 		$query = "SELECT * FROM $table_name ORDER BY time DESC";
-		$totalItems = $wpdb->query($query);
+		$total_items = $wpdb->query( $query );
 
-		if ($totalItems === false) {
+		if ( $total_items === false ) {
 			echo "<p>Error retrieving data from database: " . $wpdb->last_error . "</p>";
 		}
 
-		$data = $wpdb->get_results($wpdb->prepare("$query LIMIT %d, %d", ($currentPage - 1) * $perPage, $perPage), ARRAY_A);
+		$data = $wpdb->get_results( $wpdb->prepare( "$query LIMIT %d, %d", ( $current_page - 1 ) * $per_page, $per_page ), ARRAY_A );
 
 		$this->set_pagination_args([
-			'total_items' => $totalItems,
-			'per_page'    => $perPage
+			'total_items' => $total_items,
+			'per_page'    => $per_page
 		]);
 
 		$this->items = $data;
 	}
 
-	protected function column_default($item, $column_name) {
-		return isset($item[$column_name]) ? $item[$column_name] : 'No data';
+	protected function column_default( $item, $column_name ) {
+		return isset( $item[ $column_name ] ) ? $item[ $column_name ] : 'No data';
 	}
 	
-	protected function column_time($item) {
-		return sprintf('<strong>%s</strong>', $item['time']);
+	protected function column_time( $item ) {
+		return sprintf( '<strong>%s</strong>', $item['time'] );
 	}
 
 	protected function column_cb($item) {
