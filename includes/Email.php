2296aac600e441ba24b0c7f96cd8b2e4fed401f9
Sadekur Rahman <37816541+sadekur@users.noreply.github.com>
2024-10-18 00:21:05 +0600

    Update Email.php

--- includes/Email.php
+++ includes/Email.php
@@ -6,21 +6,39 @@ class Email {
         add_action( 'thrail_send_followup_email', [ $this, 'send_followup_email' ], 10, 2 );
     }
     public function send_congratulatory_email( $name, $email ) {
-        $subject = "Congratulations on subscribing!";
-        $message = "Hi {$name},\n\nThank you for subscribing to our newsletter!";
-        $headers = [ 'Content-Type: text/plain; charset=UTF-8' ];
+        $defaults = [
+            'congratulatory_subject' => 'Congratulations on subscribing!',
+            'congratulatory_message' => 'Hi {name},\n\nThank you for subscribing to our newsletter!',
+        ];
+        $options = get_option('thrail_crm_email_settings', $defaults);
+
+        $subject = $options['congratulatory_subject'];
+        $message = str_replace('{name}', $name, $options['congratulatory_message']);
+        $message = str_replace('\n', "\n", $message);
+        $headers = ['Content-Type: text/plain; charset=UTF-8'];
+
         wp_mail( $email, $subject, $message, $headers );
+
         $time_sent = time();
-        set_transient( 'congratulatory_email_sent_time_' . $email, $time_sent, HOUR_IN_SECONDS );
+        set_transient( 'congratulatory_email_sent_time_' . $email, $time_sent, MINUTE_IN_SECONDS );
         if ( ! wp_next_scheduled( 'thrail_send_followup_email', [ $name, $email ] ) ) {
-            wp_schedule_single_event( $time_sent + HOUR_IN_SECONDS, 'thrail_send_followup_email', [ $name, $email ] );
+            wp_schedule_single_event( $time_sent + MINUTE_IN_SECONDS, 'thrail_send_followup_email', [ $name, $email ] );
         }
     }
 
     public function send_followup_email( $name, $email ) {
-        $subject = "Follow-up: We're glad to have you!";
-        $message = "Hi {$name},\n\nIt's been a MINUTE since you subscribed! We just wanted to follow up and say thanks again.";
-        $headers = [ 'Content-Type: text/plain; charset=UTF-8' ];
+
+        $defaults = [
+            'followup_subject' => 'Follow-up: We\'re glad to have you!',
+            'followup_message' => 'Hi {name},\n\nIt\'s been a minute since you subscribed! We just wanted to follow up and say thanks again.'
+        ];
+        $options = get_option('thrail_crm_email_settings', $defaults);
+
+        $subject = $options['followup_subject'];
+        $message = str_replace('{name}', $name, $options['followup_message']);
+        $message = str_replace('\n', "\n", $message);
+        $headers = ['Content-Type: text/plain; charset=UTF-8'];
+
         wp_mail( $email, $subject, $message, $headers );
         wp_clear_scheduled_hook( 'thrail_send_followup_email', [ $name, $email ] );
         delete_transient( 'congratulatory_email_sent_time_' . $email );
